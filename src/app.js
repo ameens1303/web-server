00cf88466d91367bf77cc534f07f4785e62b281d
ameens1303 <ameen.s1303@gmail.com>
2020-04-23 23:18:21 +0530

    CUSTOMISE VIEWS PATH AND ERROR REDIRECTING PAGE ON NO ROUTE

--- src/app.js
+++ src/app.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('hbs')
 
 const app = express();
 
 const publicDirectoryPath = path.join(__dirname,'../public');
+const viewPath = path.join(__dirname,'../templates/views');
+const partialsPath = path.join(__dirname,'../templates/partials');
+
+
 
 app.use(express.static(publicDirectoryPath));
 app.set('view engine', 'hbs')
+app.set('views', viewPath)
+hbs.registerPartials(partialsPath);
+
+
 
 
 
@@ -34,6 +43,23 @@ app.get("/weather",(req, res)=>{
     res.send('<title> Weather</title><p> Display the Weather</p>')
 
 })
+
+app.get('/help/*', (req, res) => {
+    res.render('errorHelp404', {
+        title: 'Weather',
+        name: 'Andrew Mead'
+    })
+    //res.send('<title> HELP ARTICLE NOT FOUND</title><p> Display the Weather</p>')
+})
+
+app.get('*', (req, res) => {
+    res.render('error404', {
+        title: 'Weather',
+        name: 'Andrew Mead'
+    })
+})
+
+
 
 app.listen(3000, ()=>{
     console.log("start listening")
